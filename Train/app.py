a54ca17860ae4409c3998c9fbbbdc4f665b2a9b1
Blasto <75465337+Barath-RR@users.noreply.github.com>
2023-07-31 17:35:35 +0530

    Train update

--- Train/app.py
+++ Train/app.py
@@ -6,22 +6,31 @@ app = Flask(__name__)
 
 TRAIN_API_URL = "http://20.244.56.144/train/trains"
 
-BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA3OTc1NzcsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiYjM2MTRjZjYtZGEzZC00MzU4LTllNWEtM2I3ODljMWIyYzRlIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjAyMyJ9.3kKFyoShiLXWHFsFpRuL_3OwACnOYrVGjPOTkCqBSRA"
+BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA4MDQ2MzIsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiYjM2MTRjZjYtZGEzZC00MzU4LTllNWEtM2I3ODljMWIyYzRlIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjAyMyJ9.XJngQ-7kkdrNvdhPmFKZ9YORYM04VJwtvP5W0X20PDI"
 
 @app.before_request
 def before_request():
-    request.headers['Authorization'] = f'Bearer {BEARER_TOKEN}'
+    request_headers = dict(request.headers)
+    request_headers['Authorization'] = f'Bearer {BEARER_TOKEN}'
+    request.headers = request_headers
 
 @app.route('/trains', methods=['GET'])
 def get_trains():
     try:
-        response = requests.get(TRAIN_API_URL, headers=request.headers)
+        response = requests.get(TRAIN_API_URL, headers=dict(request.headers))
         response.raise_for_status()
 
         train_data = response.json()
 
+        if not isinstance(train_data, list):
+            raise ValueError("Invalid response data format. Expected a list of trains.")
+
         current_time = datetime.now()
-        filtered_trains = [train for train in train_data if calculate_departure_time(train) > current_time + timedelta(minutes=30)]
+        def dict_to_str(d):
+            return  ":".join(str(y) for y in d.values())
+        
+        next_12_hours = current_time + timedelta(hours=12)
+        filtered_trains = [train for train in train_data if train.get('departureTime') and datetime.strptime(dict_to_str(train['departureTime']), "%H:%M:%S") < next_12_hours]
 
         for train in filtered_trains:
             train['seatsAvailable'] = {
@@ -32,19 +41,22 @@ def get_trains():
                 'sleeper': train['price']['sleeper'],
                 'AC': train['price']['AC']
             }
+    #     s =""
+    # # print(departure_time_str)
+    #     for x,y in departure_time_str.items():
+    #         s += str(y)+':'
+    #     s = s[:-1]
+    # print(s)
+        # departure_time = datetime.strptime(s, "%H:%M:%S")
 
-        filtered_trains.sort(key=lambda x: (x['price']['sleeper'], -x['seatsAvailable']['sleeper'], calculate_departure_time(x)))
+        filtered_trains.sort(key=lambda x: (x['price']['sleeper'], -x['seatsAvailable']['sleeper'], datetime.strptime(dict_to_str(x['departureTime']), "%H:%M:%S")))
 
         return jsonify(filtered_trains)
 
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
 
-def calculate_departure_time(train):
-    departure_time = train['departureTime']
-    departure_delay = train['delayedBy']
-    departure_datetime = datetime(year=2023, month=7, day=31, hour=departure_time['Hours'], minute=departure_time['Minutes'], second=departure_time['Seconds'])
-    return departure_datetime + timedelta(minutes=departure_delay)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+#Given challenge with all constraints have been completed the train detail within next 12 hours will be displayed and in ascending order and other users can access this API without bearer token.
